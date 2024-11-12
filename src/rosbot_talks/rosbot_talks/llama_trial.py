d48e4047640a5f3bbcfc8b55f42226567c69aa85
Riddhi_Goswami <ridhgoswami@gmail.com>
2024-11-12 11:31:57 +0400

    using llama-3.2-3B-instruct

--- src/rosbot_talks/rosbot_talks/llama_trial.py
+++ src/rosbot_talks/rosbot_talks/llama_trial.py
@@ -1,12 +1,14 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline
 import torch
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
-input_text = "The key to life is"
-inputs = tokenizer(input_text, return_tensors="pt").to("cuda")  # Move inputs to GPU if available
+model_id = "meta-llama/Llama-3.2-3B-Instruct"
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    torch_dtype=torch.bfloat16,
+)
 
-output = model.generate(**inputs, max_new_tokens=50)
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-print(generated_text)
+messages = [{"role": "user", "content": "What are quantum computers?"}]
+outputs = pipe(messages, max_new_tokens=150)
+response = outputs[0]["generated_text"]
+print(response)
